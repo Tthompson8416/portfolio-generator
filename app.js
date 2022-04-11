dd3e4c748a60d90e7cf6244387e78c96466a3a06
TT84 <tthompson8416@outlook.com>
2022-04-11 02:11:01 -0400

    generate html file

--- app.js
+++ app.js
@@ -1,15 +1,34 @@
+const fs = require('fs');
+
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 
-const name = profileDataArgs[0];
-const github = profileDataArgs[1];
+const [name, github] = profileDataArgs;
 
-const generatePage = (userName, gitHubName) => {
+const generatePage = (name, github) => {
   return `
+<DOCTYPE html>
+<html lang="en">
 
-Name: ${userName}
-Github: ${gitHubName}
+<head>
+    <meta charset= "UTF-8">
+    <meta name= "viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-compatible" content="ie=edge">
+    <title>Portfolio Demo<title>
+</head>
 
-`;
+<body>
+    <h1>${name}</h1>
+    <h2><a href= "https://github.com/${github}">Github</a></h2>
+
+</body>
+</html>
+
+ `;
 };
 
-console.log(generatePage(name, github));
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw err;
+
+    console.log('Portfolio complete! Check out index.html to see the output!');
+
+});
